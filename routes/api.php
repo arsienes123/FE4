136b27294f18bcb7d1a4fa4fd7ef870b8d361ef4
arsienes123 <60423484+arsienes123@users.noreply.github.com>
2020-01-29 16:16:53 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::any('exercise-1', 'Exercise1');
-Route::any('exercise-2', 'Exercise2');
-Route::any('exercise-3', 'Exercise3@index');
-Route::any('exercise-3/store', 'Exercise3@store');
-Route::any('exercise-final', 'ExerciseFinal@login');
-Route::any('exercise-final/store', 'ExerciseFinal@store');
+Route::any('arnulfo', 'Exercise1');
+Route::any('arnulfo', 'Exercise2');
+Route::any('arnulfo', 'Exercise3@index');
+Route::any('arnulfo/store', 'Exercise3@store');
+Route::any('arnulfo-final', 'ExerciseFinal@login');
+Route::any('arnulfo-final/store', 'ExerciseFinal@store');
